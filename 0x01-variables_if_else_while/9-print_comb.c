cfc3962222fc2c9da525a2ea224bf3d9f3d2151e
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-08 17:15:06 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,9 @@ int main(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
