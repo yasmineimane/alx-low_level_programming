4f55de4cefd71dd848bd5cdaf00400c5facad294
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-12 08:07:48 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
