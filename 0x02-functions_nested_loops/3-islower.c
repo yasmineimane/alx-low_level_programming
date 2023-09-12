889548b3a8bc10d843d597522699cc5a5abbf149
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-12 09:16:51 -0700

    new

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 
 /**
  * _islower - cheks for lowercase character
+ * @c: parameter to be checked
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,7 @@
 
 /**
  * _isalpha - cheks for alphabetic character
+ * @c: parameter to be chacked
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
