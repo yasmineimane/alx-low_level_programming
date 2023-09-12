8e819127a7174e56dec44a6e1071274ae45ebb0c
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-12 16:55:18 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,14 @@ void times_table(void)
 		for (j = 0 ; j < 10 ; j++)
 		{
 			result = i * j;
-			printf("%d, ", result);
+			if (j == 0)
+				printf("%d, ", result);
+			else
+			{
+				printf("%2d", result);
+				if (j != 9)
+					printf(", ");
+			}
 		}
 		printf("\n");
 	}
