7e732e05bd83d4bccac872726af2b70f12160cc8
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-13 16:13:09 -0700

    100-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (i = 3 ; i <= 1024 ; i++)
 	{
-		if ((3 % i == 0 || 5 % i == 0))
+		if ((i % 3 == 0 || i % 5 == 0))
 			sum = sum + i;
 	}
-	printf("The sum of all the multiples of 3 or 5 below 1024 is : %d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
