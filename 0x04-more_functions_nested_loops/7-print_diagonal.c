e7db42f787d64cfd28a3f47262d35b108f7afefd
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-14 15:00:28 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,7 @@ void print_diagonal(int n)
 	{
 		for (i = 0 ; i < n ; i++)
 		{
-			for (j = 0 ; j <= i ; j++)
+			for (j = 0 ; j < i ; j++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
