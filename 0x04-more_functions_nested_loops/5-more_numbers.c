b2149b1a20054944a9569c31a2bf31f2a64a0cff
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-14 14:08:17 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,11 @@ void more_numbers(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 15 ; j++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
