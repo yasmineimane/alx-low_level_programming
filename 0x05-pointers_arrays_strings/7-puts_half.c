1bb618c8c4a62c425e442646eeb1fc0833963c1b
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-19 07:32:53 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,12 +18,12 @@ void puts_half(char *str)
 
 	if (count % 2 == 0)
 	{
-		for (i = (count / 2) ; str[i] != '\0' ; i++)
+		for (i = ((count - 1) / 2) ; str[i] != '\0' ; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = ((count - 1) / 2) ; str[i] != '\0' ; i++)
+		for (i = ((count - 2) / 2) ; str[i] != '\0' ; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
