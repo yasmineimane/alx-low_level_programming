03c18a9c6683d8f88def413503aac7dd5887a3df
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-19 06:09:52 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,9 @@ void print_rev(char *s)
 	int i;
 	int count = 0;
 
-	for (i = 0 ; s[i] > '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count ; i >= 0 ; i--)
+	for (i = count - 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
