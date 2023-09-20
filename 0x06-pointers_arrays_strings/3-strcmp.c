3cd1fee873075dac1eac32254d0c55ea637f4d7e
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-20 07:56:07 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: The string to be compare
+ * @s2: The string to be compare
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	n = strcmp(s1, s2);
+	return (n);
+}
