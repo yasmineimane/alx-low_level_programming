6db9cc79981059f942673d5f0a3dcb3b65d700c0
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-21 07:54:43 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char s[] = "HELLO. \n";
+	char *p;
+
+	p = rot13(s);
+	printf("%s", p);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	printf("------------------------------------\n");
+	p = rot13(s);
+	printf("%s", p);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	printf("------------------------------------\n");
+	p = rot13(s);
+	printf("%s", p);
+	printf("------------------------------------\n");
+	printf("%s", s);
+	return (0);
+}
