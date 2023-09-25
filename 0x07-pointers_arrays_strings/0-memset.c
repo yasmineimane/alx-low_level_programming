8beb7741999079a735b40fdbd6e3c1b33c5fdb79
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-25 17:35:10 +0200

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @n: Prameter to be checked
+ * @b: Parameter to be checked
+ * @s: Parameter to be checked
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    char *res;
+
+    res = memset(s, b, n);
+    return (res);
+}
