68430dc9e4e1c9d2902753056701583426eaefd5
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-25 23:06:38 +0200

    .\7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_chessboard -  function that prints the chessboard.
+ * @a: Prameter to be checked
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+    int i, j;
+
+    for (i = 0 ; i < 8 ; i++)
+    {
+        for (j = 0 ; j < 8 ; j++)
+            _putchar(a[i][j]);
+        _putchar('\n');
+    }
+}
