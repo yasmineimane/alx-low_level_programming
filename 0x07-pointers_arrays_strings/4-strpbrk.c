719ab67b7755472c1ca67837b1954c73eaad6dd5
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-25 22:29:33 +0200

    .\4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ * @accept: Prameter to be checked
+ * @s: Prameter to be checked
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+    s = strpbrk(s, accept);
+    return (s);
+}
