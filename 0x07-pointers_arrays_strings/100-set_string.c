de97bfaa663a0e4ca40c24d350d8be94026d3299
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-26 14:31:02 +0200

    new

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _set_string - function that sets the value of a pointer to a char.
+ * set_string - function that sets the value of a pointer to a char.
  * @s: Prameter to be checked
  * @to: Parameter to be checked
+ * 
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
