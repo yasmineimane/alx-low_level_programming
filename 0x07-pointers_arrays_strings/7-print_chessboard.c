3e293732d6b0642ce808887dcf62d5419ec975e5
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-26 14:25:42 +0200

    new

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,8 +14,8 @@ int i, j;
 
 for (i = 0 ; i < 8 ; i++)
 {
-    for (j = 0 ; j < 8 ; j++)
-        _putchar(a[i][j]);
-    _putchar('\n');
+for (j = 0 ; j < 8 ; j++)
+    _putchar(a[i][j]);
+_putchar('\n');
 }
 }
