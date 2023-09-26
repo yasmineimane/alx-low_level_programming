f97fc11094186fc4dd7d8e72a0ca46eb86d6b339
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-26 14:28:16 +0200

    new

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,11 +16,11 @@ int i, sum1 = 0, sum2 = 0;
 
 for (i = 0 ; i < size ; i++)
 {
-    sum1 += a[i * size + i];
+sum1 += a[i * size + i];
 }
 for (i = size - 1 ; i >= 0 ; i--)
 {
-    sum2 += a[i * size + (size - i - 1)];
+sum2 += a[i * size + (size - i - 1)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
