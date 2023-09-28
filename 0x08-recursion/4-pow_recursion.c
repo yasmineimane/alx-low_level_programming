f5650df33e17edf25d5698e1216e3810b98b4739
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-28 21:51:30 +0200

    new

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,17 +9,17 @@
 
 int _pow_recursion(int x, int y)
 {
-    if ( y < 0)
-    {
-        return(-1);
-    }
-    else if (y == 0)
-    {
-        return(1);
-    }
-    else
-    {
-        return(x * _pow_recursion(x, (y - 1)));
-    }
-    return(0);
+if (y < 0)
+{
+return(-1);
+}
+else if (y == 0)
+{
+return(1);
+}
+else
+{
+return(x * _pow_recursion(x, (y - 1)));
+}
+return(0);
 }
