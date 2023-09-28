d3c3b84ab1bc1c210c01a9e0a6a8b201997f3aed
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-29 01:43:29 +0200

    new

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -24,7 +24,7 @@ if (n <= 1)
 {
 return (0);
 }
-if(n % i == 0 && i > 1)
+if (n % i == 0 && i > 1)
 {
 return (0);
 }
@@ -32,5 +32,5 @@ if ((n / i) < i)
 {
 return (1);
 }
-return (check_prime(n, i+ 1));
+return (check_prime(n, i + 1));
 }
