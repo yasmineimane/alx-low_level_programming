00bf1704076dbde4b869539e0af7753ba84ace6c
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-28 21:01:36 +0200

    new

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number.
+ * @n: Parameter to be checked.
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+
+int factorial(int n)
+{
+if (n == 0)
+    return (1);
+else
+    return (n * factorial(n - 1));
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+int factorial(int n);
+
+#endif
