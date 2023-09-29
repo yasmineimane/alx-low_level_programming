4baba65f9fecbf93e25dda31bfd541d23f2992d9
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-29 14:53:00 +0200

    new

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,5 +8,23 @@
 
 int _sqrt_recursion(int n)
 {
-    
+return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - return the square root.
+ * @n: natural number.
+ * @i: counter.
+ * Return: square root.
+*/
+
+int _sqrt(int n, int i)
+{
+int sqr = i * i;
+
+if (sqr > n)
+return (-1);
+if (sqr == n)
+return (i);
+return (_sqrt(n, i + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int check_prime(int n, int i);
 int is_prime_number(int n);
+int _sqrt(int n, int i);
 int _sqrt_recursion(int n);
 void _print_rev_recursion(char *s);
 int count_length(char *s, int i);
