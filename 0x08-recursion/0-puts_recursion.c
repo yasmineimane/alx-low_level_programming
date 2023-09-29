9122702906527d9d08631213d89f01108855d366
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-09-29 14:11:58 +0200

    new

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,9 +2,7 @@
 
 /**
  *_puts_recursion - unction that prints a string, followed by a new line.
- *
  * @s: The string to be print.
- * 
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
 */
@@ -12,7 +10,10 @@
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
+{
+_putchar('\n');
 return;
+}
 _putchar(*s);
 _puts_recursion(s + 1);
 }
