5a179448e4bcd76f823c1875513fd9e3cbd5901a
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-10-05 00:08:30 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(char)*size);
+	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (NULL);
