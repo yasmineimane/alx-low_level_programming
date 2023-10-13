196c5c2cc98e25fe76c8c8d2fcc02cdf97ac926e
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-10-13 12:00:55 +0200

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,6 +28,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	printf ("\n");
+	printf("\n");
 	va_end(strings);
 }
