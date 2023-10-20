52bedec9a1aa886f6b57ab4e8969ec4e722cf82e
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-10-20 12:56:19 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * ch_free_grid - main entry
+ * @grid: input
+ * @height: input
+ */
+
+void ch_free_grid(char **grid, size_t height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0 ; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
+
+/**
+ * strtow - splits string into two
+ * @str: string
+ * Return: a pointer to the new allocated memory for the string
+ */
+
+char **strtow(char *str)
+{
+	char **aout;
+	size_t c, height, i, j, a1;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (c = height = 0 ; str[c] != '\0' ; c++)
+	{
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			height++;
+	}
+	aout = malloc(sizeof(char *) * (height + 1));
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+	for (i = a1 = 0 ; i < height ; i++)
+	{
+		for (c = a1 ; str[c] != '\0' ; c++)
+		{
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
+				{
+					ch_free_grid(aout, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (j = 0 ; a1 <= c ; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
+	}
+	aout[i] = NULL;
+	return (aout);
+}
