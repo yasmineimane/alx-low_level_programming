c93cdcbe87d6fc70ef1e8f274c7baad1fa8dad9b
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-10-24 15:26:41 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	for (i = 0 ; i < index - 1 && temp != NULL && index != 0 ; i++)
-		temp = (*head)->next;
+		temp = temp->next;
 	if (temp == NULL)
 		return (-1);
 	if (index == 0)
