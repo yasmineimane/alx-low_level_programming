bd6446472c3341ccfc6a536d01940d93eded65f4
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2023-10-27 10:06:02 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 		;
 
 	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-			return (b[0] - 48);
+		return (b[0] - 48);
 
 	for (i = 0 ; b[i] != '\0' ; i++)
 	{
