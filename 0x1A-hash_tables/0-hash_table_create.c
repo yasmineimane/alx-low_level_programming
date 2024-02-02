805ce24821d72e801435ce9670a0849588b29386
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-02-02 07:45:38 -0800

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash;
+
+	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+		return (NULL);
+	new_hash->size = size;
+	new_hash->array = calloc(size, sizeof(hash_node_t));
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+
+	return (new_hash);
+}
